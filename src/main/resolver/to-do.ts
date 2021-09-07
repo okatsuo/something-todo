5ba0dec44039c4d4fe75c58a1c8550acf1824501
okatsuo <rafael.goncanna@gmail.com>
2021-09-06 21:09:40 -0300

    refactor: change todoCreate to Mutation instead Query

--- src/main/resolver/to-do.ts
+++ src/main/resolver/to-do.ts
@@ -1,15 +1,15 @@
-import { Arg, Query, Resolver } from 'type-graphql'
+import { Arg, Mutation, Resolver } from 'type-graphql'
 import { makeTodoController } from '../factory/add-to-do'
 import { TodoSchema } from '../schema/to-do'
 
 @Resolver()
 export class TodoResolver {
-  @Query(() => TodoSchema)
+  @Mutation(() => TodoSchema)
   todoCreate (
-    @Arg('name') name: string,
+    @Arg('user_id') userId: string,
+      @Arg('name') name: string,
       @Arg('description', { nullable: true }) description: string,
-      @Arg('user_id') userId: string,
-      @Arg('active', { nullable: true }) active: boolean
+      @Arg('active') active: boolean
   ): any {
     const todoController = makeTodoController()
     return todoController.handle({ name, user_id: userId, active, description })
