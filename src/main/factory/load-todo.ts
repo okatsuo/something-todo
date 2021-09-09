9f78ac3f59797facc639fbcd4818665ec2e1c590
okatsuo <rafael.goncanna@gmail.com>
2021-09-09 20:30:02 -0300

    feat: make load todo by user id controller factory

---
+++ src/main/factory/load-todo.ts
@@ -0,0 +1,9 @@
+import { LoadTodo } from '../../data/usecases/todo/list-todo'
+import { TodoRepository } from '../../infra/db/mongodb/todo-repository/todo'
+import { LoadTodoController } from '../../presentation/todo/load-todo-controller'
+
+export const makeLoadTodoByUserIdController = (): LoadTodoController => {
+  const loadTodoRepository = new TodoRepository()
+  const loadTodo = new LoadTodo(loadTodoRepository)
+  return new LoadTodoController(loadTodo)
+}
