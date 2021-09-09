5e500c970e193d526af9b9533acf81a63ba98c2b
okatsuo <rafael.goncanna@gmail.com>
2021-09-09 20:24:53 -0300

    feat: loadTodoByUser repository

--- src/infra/db/mongodb/todo-repository/todo.ts
+++ src/infra/db/mongodb/todo-repository/todo.ts
@@ -1,11 +1,18 @@
 import { TodoModel } from '../../../../domain/models/to-do'
 import { IAddTodo, IDbAddTodo } from '../../../../domain/usecases/add-to-do'
+import { ILoadDbTodo, ILoadTodo } from '../../../../domain/usecases/loadTodo'
 import { TodoEntity } from '../../entity/to-do'
 
-export class TodoRepository implements IDbAddTodo {
+export class TodoRepository implements IDbAddTodo, ILoadDbTodo {
   async add (todoData: IAddTodo): Promise<TodoModel> {
     const todo = TodoEntity.create(todoData)
     await todo.save()
+    return todo
+  }
+
+  async loadTodoByUser (todoData: ILoadTodo): Promise<TodoModel> {
+    const { user_id } = todoData
+    const todo = await TodoEntity.findOneOrFail({ where: { user_id } })
     return todo
   }
 }
