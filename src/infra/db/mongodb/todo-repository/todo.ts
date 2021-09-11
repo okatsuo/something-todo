8c13aff63c7b96bce4f7169430fcd8f135d7dcc8
okatsuo <rafael.goncanna@gmail.com>
2021-09-11 10:17:11 -0300

    feat: ensure updateTodo repository return with correct values

--- src/infra/db/mongodb/todo-repository/todo.ts
+++ src/infra/db/mongodb/todo-repository/todo.ts
@@ -1,9 +1,10 @@
 import { TodoModel } from '../../../../domain/models/to-do'
 import { IAddTodo, IDbAddTodo } from '../../../../domain/usecases/add-to-do'
 import { ILoadDbTodo, ILoadTodo } from '../../../../domain/usecases/loadTodo'
+import { IUpdateTodo, IUpdateTodoDb } from '../../../../domain/usecases/update-todo'
 import { TodoEntity } from '../../entity/to-do'
 
-export class TodoRepository implements IDbAddTodo, ILoadDbTodo {
+export class TodoRepository implements IDbAddTodo, ILoadDbTodo, IUpdateTodoDb {
   async add (todoData: IAddTodo): Promise<TodoModel> {
     const todo = TodoEntity.create(todoData)
     await todo.save()
@@ -13,6 +14,12 @@ export class TodoRepository implements IDbAddTodo, ILoadDbTodo {
   async loadTodoByUser (todoData: ILoadTodo): Promise<TodoModel[]> {
     const { user_id } = todoData
     const todo = await TodoEntity.find({ where: { user_id } })
+    return todo
+  }
+
+  async update (todoData: IUpdateTodo): Promise<TodoModel> {
+    const { todo_id, fields } = todoData
+    const todo = await TodoEntity.update(todo_id, { ...fields }).then(response => response.raw[0])
     return todo
   }
 }
