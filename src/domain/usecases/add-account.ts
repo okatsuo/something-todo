838f32fd7555194acdf3e2d137e5b407ec65ae89
okatsuo <rafael.goncanna@gmail.com>
2021-10-16 19:59:54 -0300

    refactor: change presentation tests to new inputs type

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -4,7 +4,9 @@ export interface IAddAccount {
   name: string
   email: string
   password: string
+  active: boolean | null
 }
+
 export interface IDbAddAccount {
   add: (account: IAddAccount) => Promise<Account>
 }
