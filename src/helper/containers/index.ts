caf9931fc5574a61a6286273d9d32f04e34e50a8
okatsuo <rafael.goncanna@gmail.com>
2021-10-16 23:55:33 -0300

    style: change folder name

--- src/helper/containers/index.ts
+++ src/helper/containers/index.ts
@@ -1,6 +1,6 @@
 
 import { registerDatabaseContainers } from './database-containers'
-import { registerInfraContainers } from './repository-containers'
+import { registerInfraContainers } from './infra-containers'
 import { registerValidationContainer } from './validators-container'
 
 export const registerContainers = (): void => {
